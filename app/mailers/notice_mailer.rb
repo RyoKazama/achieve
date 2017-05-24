772ca07c539cdd61ae093548940c059039a415b2
風間亮 <ryokazama@kazamaryou-no-MacBook-Air.local>
2017-05-24 18:36:03 +0900

    メール送り先変更2

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
     def sendmail_contact(contact)
       @contact = contact
 
-      mail to: @blog.email,
+      mail to: @contact.email,
       subject:'[Achieve]お問い合わせ完了しました'
     end
 end
