9c6cfc2c18905d486bb51e721e0a3c8b229a795a
風間亮 <ryokazama@kazamaryou-no-MacBook-Air.local>
2017-06-27 20:17:42 +0900

    メッセージの追加

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,10 @@ end
 
   resources :relationships, only: [:create, :destroy]
 
+  resources :conversations do
+      resources :messages
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
